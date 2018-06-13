f1817d249b5c40a54c98164d68d413f0803c557b
artem88 <artemik2010@gmail.com>
2018-06-13 21:59:51 +0300

    Создан и настроен проект в Unity

---
+++ Assets/Scripts/WaitOneSecond.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitOneSecond : MonoBehaviour {
+
+    Rigidbody _rigidbody;
+    public float seconds;
+    public float massOfObject;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(MyCoroutine());
+    }
+
+    
+    IEnumerator MyCoroutine()
+    {
+        yield return new WaitForSeconds(seconds);
+
+        _rigidbody = gameObject.AddComponent<Rigidbody>(); 
+        _rigidbody.mass = massOfObject;
+    }
+}
